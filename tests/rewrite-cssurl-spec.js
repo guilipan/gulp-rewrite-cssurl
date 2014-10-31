43327c5d9170981a21f7e599b3809484d5ecb95c
shaynegui <shaynegui@tencent.com>
2014-10-31 20:28:17 +0800

    增加传入stream的测试用例

--- tests/rewrite-cssurl-spec.js
+++ tests/rewrite-cssurl-spec.js
@@ -2,7 +2,8 @@ var chai = require("chai");
 var expect = chai.expect;
 var gutil = require("gulp-util");
 var File = gutil.File;
-
+var path = require("path");
+var fs = require("fs");
 var rewriteCSSURL = require("../index");
 
 describe("gulp-rewrite-cssurl,用户给css中的url重写前缀", function () {
@@ -26,4 +27,32 @@ describe("gulp-rewrite-cssurl,用户给css中的url重写前缀", function () {
 
         })
     })
+
+    it("如果css的url中已经是个绝对地址,提取最后的文件名并合并前缀",function(done){
+        //todo sdfsdf
+    })
+
+    it("传入的文件是stream,抛出异常", function (done) {
+
+        var streamPath = path.join(__dirname, "fixtures/a.css");
+
+        var fakeFile = new File({
+            base: "/test/",
+            path: "/test/a.css",
+            contents: fs.createReadStream(streamPath)
+        });
+
+        var stream = rewriteCSSURL();
+
+        stream.on("error", function (error) {
+            expect(error).to.be.an.instanceOf(gutil.PluginError);
+            expect(error.message).to.include("streaming not supported");
+            done();
+        })
+
+
+        stream.write(fakeFile);
+    })
+
+
 })
